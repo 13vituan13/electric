438002801ae4cacfcd706be8711bab1377a73099
Vi Tuan <qvituan13@gmail.com>
2023-07-20 22:39:15 +0700

    update code

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'electric' );
+define( 'DB_NAME', 'if0_34640060_electric' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'if0_34640060' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'ZaMAwhOiNBXRf' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'sql304.infinityfree.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
