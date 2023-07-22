e9f100e2a8c8b645d5bf2580110a80feb9aef6c3
PhanQuyen <quyenpc@hp-backup.com>
2023-07-22 14:39:12 +0700

    edit gg tag

--- wp-content/themes/electricTheme/header.php
+++ wp-content/themes/electricTheme/header.php
@@ -45,29 +45,21 @@
     <link href="<?php bloginfo('template_directory') ?>/css/animate.css" rel="stylesheet">
     <link href="<?php bloginfo('template_directory') ?>/css/all.css" type="text/css" rel="stylesheet" />
     
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4YMJWZ9P7W"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-        gtag('config', 'G-4YMJWZ9P7W');
-    </script>
-    <meta name="google-site-verification" content="OLn77-5DjWCdtLk1E_RA6HXbyXUq_vJbR-rXsr9S-Ls" />
-    <meta name="google-site-verification" content="74iAwgT_3dU2-1JeniNP1RXcn-wJabqMS5rINxURdl0" />
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NJM5JHGF');</script>
+    <!-- End Google Tag Manager -->
     <?php wp_head(); ?>    
 </head>
 
 <body>
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4YMJWZ9P7W"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-
-        gtag('config', 'G-4YMJWZ9P7W');
-    </script>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJM5JHGF"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <div id="wapper">
         <div class="header">
             <div class="w_hd lazy bgbg" data-bg="url('<?php bloginfo('template_directory') ?>/images/background/bg_hd-5083.png')">
